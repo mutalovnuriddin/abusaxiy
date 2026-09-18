2c930a5b0370d4b4b453e5d09035c432e716a0b3
nuriddin <mutalovnuriddin651@gmail.com>
2026-09-18 18:05:12 +0500

    update host

--- config/settings.py
+++ config/settings.py
@@ -20,7 +20,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://abusaxiy.onrender.com']
 
 CSRF_TRUSTED_ORIGINS = [origin.strip() for origin in os.getenv('CSRF_TRUSTED_ORIGINS', '').split(',') if origin.strip()]
 
